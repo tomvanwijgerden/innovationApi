0312d15a067e9ffd0912734ce23f8532cac98e55
Tom van Wijgerden <tom.vanwijgerden@visma.com>
2022-01-11 14:16:38 +0100

    return array with user data for login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,7 +34,10 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
-            'user' => $user,
+            'user' => [
+                'name' => $user->name,
+                'email' => $user->email
+            ],
         ]);
     }
 
